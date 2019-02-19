e4bd3cd83c01f94d99f1e99660fa45c143183c7a
afonsky <alexey.boldyrev@gmail.com>
2019-02-20 00:36:38 +0300

    Fix LHCb SW initialisation error

--- Docker_build/script.sh
+++ Docker_build/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 /usr/bin/cubied
 python3.6 change_tcl.py
-/cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v9r2p4/InstallArea/scripts/LbLogin.sh -c x86_64-centos7-gcc62-opt
+. /cvmfs/lhcb.cern.ch/lib/lhcb/LBSCRIPTS/LBSCRIPTS_v9r2p7/InstallArea/scripts/LbLogin.sh -c x86_64-centos7-gcc62-opt
+lb-project-init
 cd Gauss/Sim/LbDelphes/options && /home/nb_user/Gauss/build.x86_64-centos7-gcc62-opt/run gaudirun.py Gauss-Job.py
 python3.6 get_std.py

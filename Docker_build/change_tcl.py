1e90cc21f9a000282c381622635ca0ae43b30aaa
matyushinleonid <matyushinleonid@users.noreply.github.com>
2019-03-01 16:17:30 +0300

    one-dimensional optimization

--- Docker_build/change_tcl.py
+++ Docker_build/change_tcl.py
@@ -4,24 +4,26 @@ from pathlib import Path
 
 logs_path = Path(os.environ['LOGS_DIR'])
 input_file = logs_path / "input.txt"
-if not input_file.exists:
-	raise Exception("No input file {}". format(logs_path / "input.txt"))
+if not input_file.exists():
+    raise Exception("No input file {}". format(logs_path / "input.txt"))
 
 with open(input_file, "r") as f:
-	param1, param2 = f.readline().split()
-param1 = float(param1)
-param2 = float(param2)
+    param1 = f.readline()
+    
+param1 = int(param1)
+param2 = param1 // 3 * 4
 
-temp1, temp2 = (727.2 * 2) / param1, (969.6 * 2) / param2
+temp = (727.2 * 2) / param1 # also it is equal to (969.6 * 2) / param2
+
 fname = './Gauss/Sim/LbDelphes/options/cards/delphes_card_LHCb_EndVelo_Rich2_withEcal.tcl'
 
 with open(fname, 'r') as file:
     data = file.readlines()
 
 data[365] = '   for {set i 0} {$i <= ' + str(param1) + '} {incr i} {\n'
-data[366] = '       add YBins [expr { -727.2 + $i * ' + str(temp1) + ' }]\n'
+data[366] = '       add YBins [expr { -727.2 + $i * ' + str(temp) + ' }]\n'
 data[368] = '   for {set i 0} {$i <= ' + str(param2) + '} {incr i} {\n'
-data[369] = '       set X [expr { -969.6 + $i * ' + str(temp2) + ' }]\n'
+data[369] = '       set X [expr { -969.6 + $i * ' + str(temp) + ' }]\n'
 
 with open(fname, 'w') as file:
     file.writelines(data)

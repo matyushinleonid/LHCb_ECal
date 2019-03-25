11046bc8c73f7763d50076328dd37e64c6a28cdf
Igor <igor.musinov@phystech.edu>
2019-03-25 14:12:09 +0300

    WIP parallel

--- Docker_build/change_tcl.py
+++ Docker_build/change_tcl.py
@@ -2,16 +2,26 @@ import sys
 import os
 from pathlib import Path
 
-logs_path = Path(os.environ['LOGS_DIR'])
-input_file = logs_path / "input.txt"
-if not input_file.exists():
-    raise Exception("No input file {}". format(logs_path / "input.txt"))
-
-with open(input_file, "r") as f:
-    param1 = f.readline()
+def get_param_file():
+    logs_path = Path(os.environ['LOGS_DIR'])
+    input_file = logs_path / "input.txt"
+    if not input_file.exists():
+        raise Exception("No input file {}".format(logs_path / "input.txt"))
+        
+    with open(input_file, "r") as f:
+        param1 = float(f.readline())
+        param2 = float(f.readline())
+        
+    return param1, param2
     
-param1 = int(param1)
-param2 = param1 // 3 * 4
+def get_param():
+    param1 = os.getenv('PARAM1', None)
+    if param1:
+        return int(param1)
+    else: 
+        return int(get_param_file())
+       
+param1, param2 = get_param_file()
 
 temp = (727.2 * 2) / param1 # also it is equal to (969.6 * 2) / param2
 
